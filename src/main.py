d7133829251b56641610b670039930ccb8e3f619
FRANK AMADI <frankitchy@FRANKs-MacBook-Pro.local>
2025-07-10 12:47:59 +0100

    Update: change python settings to work with railway

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 import os
 import sys
-# DON\'T CHANGE THIS !!!
+# DON'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
 from flask import Flask, send_from_directory
 from src.routes.booking import booking_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
-
 app.register_blueprint(booking_bp, url_prefix='/api')
 
 @app.route('/', defaults={'path': ''}) 
@@ -16,8 +14,7 @@ app.register_blueprint(booking_bp, url_prefix='/api')
 def serve(path):
     static_folder_path = app.static_folder
     if static_folder_path is None:
-            return "Static folder not configured", 404
-
+        return "Static folder not configured", 404
     if path != "" and os.path.exists(os.path.join(static_folder_path, path)):
         return send_from_directory(static_folder_path, path)
     else:
@@ -27,8 +24,8 @@ def serve(path):
         else:
             return "index.html not found", 404
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-
+    # Get port from environment variable (Railway sets this)
+    port = int(os.environ.get('PORT', 5000))
+    # Set debug to False for production deployment
+    app.run(host='0.0.0.0', port=port, debug=False)
